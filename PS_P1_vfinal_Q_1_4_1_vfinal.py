e2b78d447fd7bfb75fec05ea1c3724b053655aa8
rafaelcpenha <83182707+rafaelcpenha@users.noreply.github.com>
2024-02-29 15:30:43 -0300

    Update and rename PS_P1_vfinal_Q_1_4_vfinal.py to PS_P1_vfinal_Q_1_4_1_vfinal.py

--- PS_P1_vfinal_Q_1_4_1_vfinal.py
+++ PS_P1_vfinal_Q_1_4_1_vfinal.py
@@ -14,28 +14,3 @@ if nome in competidores:
     print(f"{nome} está na lista de competidores.")
 else:
     print(f"{nome} não está na lista de competidores. Lembre-se de que letras maiúsculas são diferenciadas de minúsculas.")
-
-
-# 2 - Crie um dicionário do Python com as seguintes informações do formato chave-valor:
-	#Joao - Competicao
-    #Carlos - Desenvolvimento
-    #Patricia - Competicao
-    #Caio - Competicao
-    #Leticia - Gestao
-
-	# E em seguida peça ao usuário um nome e imprima qual a equipe a pessoa participa. Se o nome não existir na lista, imprima que o nome não existe.
-
-equipes = {
-    'João': 'Competição',
-    'Carlos': 'Desenvolvimento',
-    'Patricia': 'Competição',
-    'Caio': 'Competição',
-    'Leticia': 'Gestão'
-}
-
-nome = input("Digite um nome para verificar a equipe: ")
-
-if nome in equipes:
-    print(f"{nome} participa da equipe de {equipes[nome]}.")
-else:
-    print(f"{nome} não está na lista de participantes. Lembre-se de que letras maiúsculas são diferenciadas de minúsculas.")
